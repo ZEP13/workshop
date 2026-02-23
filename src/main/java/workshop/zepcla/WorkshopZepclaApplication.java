bd2752369f7d8fae5199825acb7d14099e9d1051
ZEP13 <zephyr.lathuy.zpl@gmail.com>
2026-02-23 20:12:31 +0100

    base project

---
+++ src/main/java/workshop/zepcla/WorkshopZepclaApplication.java
@@ -0,0 +1,13 @@
+package workshop.zepcla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopZepclaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopZepclaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/workshop/zepcla/WorkshopZepclaApplicationTests.java
@@ -0,0 +1,13 @@
+package workshop.zepcla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopZepclaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
