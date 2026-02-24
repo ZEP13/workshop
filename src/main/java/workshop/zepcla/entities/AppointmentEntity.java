c2d6e71eb0d52dfeb9e451b6ae4a3f20013372dd
Claris-FF15 <clarisse.binard@outlook.fr>
2026-02-24 19:10:34 +0100

    add AppointmentEntity

---
+++ src/main/java/workshop/zepcla/entities/AppointmentEntity.java
@@ -0,0 +1,39 @@
+package workshop.zepcla.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@Table(name = "appointment")
+public class AppointmentEntity extends BaseEntity{
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @Column(nullable = false)
+    private LocalTime time;
+
+    @Column(nullable = false)
+    private Long id_client;
+
+    @Column(nullable = false)
+    private Long id_users;
+
+    @Column(nullable = false)
+    private String status;
+
+}
