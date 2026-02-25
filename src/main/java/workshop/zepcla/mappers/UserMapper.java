2da3e425cf979941bd71bf406f8962406e2959f2
ZEP13 <zephyr.lathuy.zpl@gmail.com>
2026-02-25 11:22:03 +0100

    user sevice and usermaaper correction
    si mapper appelle un record, le new UserDto(...arg..) autrement erreur
    car record ne peut etre viee

--- src/main/java/workshop/zepcla/mappers/UserMapper.java
+++ src/main/java/workshop/zepcla/mappers/UserMapper.java
@@ -1,25 +1,25 @@
 package workshop.zepcla.mappers;
 
+import org.springframework.stereotype.Component;
+
 import workshop.zepcla.dto.userDto.UserCreationDto;
 import workshop.zepcla.dto.userDto.UserDto;
 import workshop.zepcla.dto.userDto.UserLoginDto;
 import workshop.zepcla.entities.UserEntity;
 
+@Component
 public class UserMapper {
     public UserDto toDto(UserEntity user) {
         if (user == null) {
             return null;
         }
-        UserDto userDto = new UserDto();
-        userDto = new UserDto(
+        return new UserDto(
                 user.getId(),
                 user.getEmail(),
                 user.getUsername(),
                 user.getFirstname(),
                 user.getLastname(),
                 user.getPhone());
-        return userDto;
-
     }
 
     public UserEntity toEntity(UserDto userDto) {
--- src/main/java/workshop/zepcla/services/UserService.java
+++ src/main/java/workshop/zepcla/services/UserService.java
@@ -3,19 +3,24 @@ package workshop.zepcla.services;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import lombok.AllArgsConstructor;
+import workshop.zepcla.dto.userDto.UserCreationDto;
 import workshop.zepcla.entities.UserEntity;
+import workshop.zepcla.exceptions.userException.UserAlreadyExistsException;
 import workshop.zepcla.exceptions.userException.UsernameNotFoundException;
+import workshop.zepcla.mappers.UserMapper;
 import workshop.zepcla.repositories.UserRepository;
 
 @AllArgsConstructor
 @Service
 public class UserService implements UserDetailsService {
 
+    private final UserMapper userMapper;
     private final UserRepository repo;
-    // private final PasswordEncoder passwordEncoder;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String username) {
@@ -30,6 +35,16 @@ public class UserService implements UserDetailsService {
                 .roles(roles)
                 .build();
 
+    }
+
+    public void save(UserCreationDto userCreationDto) {
+        if (repo.existsByUsername(userCreationDto.username())) {
+            throw new UserAlreadyExistsException("Username already exists");
+        }
+
+        UserEntity newUser = userMapper.toEntityForCreation(userCreationDto);
+        newUser.setPassword(passwordEncoder.encode(userCreationDto.password()));
+        repo.save(newUser);
     }
 
 }
