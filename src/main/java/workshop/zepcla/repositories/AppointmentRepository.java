bde2154ee0bef0f05cec9d0fc16941efcc12a336
Claris-FF15 <clarisse.binard@outlook.fr>
2026-02-25 13:16:20 +0100

    AppointmentRepository

--- src/main/java/workshop/zepcla/repositories/AppointmentRepository.java
+++ src/main/java/workshop/zepcla/repositories/AppointmentRepository.java
@@ -2,15 +2,22 @@ package workshop.zepcla.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import workshop.zepcla.entities.AppointmentEntity;
 
-import java.util.Optional;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
 
 @Repository
 public interface AppointmentRepository extends JpaRepository<AppointmentEntity, Long> {
 
-    Optional<AppointmentEntity> findById(Long id);
+    boolean existsByDateAndTime(LocalDate date, LocalTime time);
 
-    boolean existsById(Long id);
+    boolean existsByIdClientAndDateAndTime(Long id_client, LocalDate date, LocalTime time);
+
+    List<AppointmentEntity> findByDate(LocalDate date);
+
+    List<AppointmentEntity> findByIdClient(Long id_client);
+
+    List<AppointmentEntity> findByIdCreator(Long id_creator);
 }
